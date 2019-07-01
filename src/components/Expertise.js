e607a3db39a4a75f49e4a88c26c780c6aa228be1
ollinieminen <otn987@gmail.com>
2019-07-01 16:32:03 -0700

    add icons to expertise

--- src/components/Expertise.js
+++ src/components/Expertise.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { expertise } from '../text/expertise';
+import { 
+  FaRandom,
+  FaGlobeAmericas,
+  FaBookReader,
+  FaRegComments,
+  FaRegFileAlt
+} from 'react-icons/fa';
+
 
 export default function Expertise(){
 
+  const expertise = [
+    {
+      text:'Ability to quickly grasp complex solutions',
+      icon: <FaRandom/>
+    },
+    {
+      text:'Extensive experience in international business',
+      icon: <FaGlobeAmericas/>
+    },
+    {
+      text:'Ability to translate tech talk into client\'s laungage',
+      icon: <FaBookReader/>
+    },
+    {
+      text:'Native in English and finnish',
+      icon: <FaRegComments/>
+    },
+    {
+      text:'Copywrite and layout design',
+      icon: <FaRegFileAlt/>
+    }
+  ]
+  ;
+  
+
   const expertiseList = expertise.map((expertise, i)=>{
-    return <li key={i}>{expertise.text}</li>;
+    return <li key={i}>
+      <section>
+        {expertise.icon}
+        {expertise.text}
+      </section>
+    </li>;
   });
 
   return (
