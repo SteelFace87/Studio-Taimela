b750527d3791a348aa83b2b0f72c41314cdd039b
ollinieminen <otn987@gmail.com>
2019-07-02 11:27:27 -0700

    add google fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google:{
+    families:['Roboto']
+  }
+});
 
 render(
   <App />,
