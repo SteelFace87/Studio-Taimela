42bcb910edb5777945446f7dfc73c467414cc67f
ollinieminen <otn987@gmail.com>
2019-07-02 10:06:23 -0700

    basic footer is done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Footer.css';
+import { footerContent } from '../text/footer';
 
 export default function Footer(){
+  const contentList = footerContent.map((item, i)=>{
+    return (
+      <li key={i}>
+        {item}
+      </li>
+    );
+  });
   return (
     <footer className={styles.footer}>
-      
-      
+      <ul>
+        {contentList}
+      </ul>
     </footer>
   );
 }
---
+++ src/text/footer.js
@@ -0,0 +1,9 @@
+const copyright = 'Copyright @ Studio Taimela';
+const designedBy = 'Designed By: Olli Nieminen';
+const dataCollection = 'This website does not collect personal information';
+export const footerContent = [
+  copyright,
+  designedBy,
+  dataCollection
+];
+
