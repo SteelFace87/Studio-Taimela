ef0d761dafb17d2b509c049606139bae35ddd8eb
ollinieminen <otn987@gmail.com>
2019-07-08 18:05:26 -0700

    basic splas page mobie styling

--- src/components/nav/MobileNav.js
+++ src/components/nav/MobileNav.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import logo from '../../assets/images/logo.png';
 import { FaBars } from 'react-icons/fa';  
 import styles from './MobileNav.css';
 
-export default function MobileNav() {
-  return (
-    <nav className={styles.nav}>
-      <section className={styles.navBar}>
-        <img src={logo}></img>
-        <FaBars size='2rem'/>
-      </section>
-     
-    </nav>
-  );
+export default class MobileNav extends PureComponent{
+  state={
+    clicked:false
+  }
+  render(){
+    const mobileList = <li>hi</li>;
+    return ( 
+      <nav className={styles.nav}>
+        <section className={styles.navBar}>
+          <img src={logo}></img>
+          <FaBars size='2rem'/>
+        </section>
+        {this.state.clicked === true ? <ul>{mobileList}</ul> : null}
+      </nav>
+    );
+  }
 }
+
