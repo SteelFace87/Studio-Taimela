1ec8ce6100f97b05482570960a0b897553686398
ollinieminen <otn987@gmail.com>
2019-07-09 14:41:02 -0700

    add animation

--- src/components/nav/MobileNav.js
+++ src/components/nav/MobileNav.js
@@ -1,32 +1,34 @@
 import React, { PureComponent } from 'react';
 import logo from '../../assets/images/logo.png';
-import { FaBars } from 'react-icons/fa';  
+import { FaBars } from 'react-icons/fa';
 import styles from './MobileNav.css';
 import createLinks from './createLinks';
 import { allLinks } from '../../text/nav';
 
 
-export default class MobileNav extends PureComponent{
-  state={
-    clicked:false
+export default class MobileNav extends PureComponent {
+  state = {
+    clicked: false
   }
-  onClick = ()=>{
-    this.setState((state)=>{
-      return { clicked:!state.clicked };
+  onClick = () => {
+    this.setState((state) => {
+      return { clicked: !state.clicked };
     });
   }
-  render(){
+  render() {
     const mobileList = createLinks(allLinks, this.onClick);
-    return ( 
-      <nav className={styles.nav}>
-        <section className={styles.navBar}>
-          <img src={logo}></img>
-          <FaBars onClick={this.onClick} size={32}/>
-        </section>
-        <section className={styles.navMenu}>
+    return (
+      <>
+        <nav className={styles.nav}>
+          <section className={styles.navBar}>
+            <img src={logo}></img>
+            <FaBars onClick={this.onClick} size={32} />
+          </section>
+        </nav>
+        <section className={[styles.navMenu, this.state.clicked].join(' ')}>
           {this.state.clicked === true ? <ul>{mobileList}</ul> : null}
         </section>
-      </nav>
+      </>
     );
   }
 }
