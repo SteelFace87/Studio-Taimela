9554f77307388e5c3c07c56b76e4d30027b21935
ollinieminen <otn987@gmail.com>
2019-07-11 16:21:24 -0700

    start implementing TypeScript

---
+++ src/components/nav/MobileNav.tsx
@@ -0,0 +1,12 @@
+
+import * as React from 'react'
+const FunctionComponent: React.FC = () => {
+  const [navState, setNavState] = React.useState( {
+    display:false
+  }) 
+
+  
+  return(<h1>hi</h1>)
+}
+
+export default FunctionComponent;
