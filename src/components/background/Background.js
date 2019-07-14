285a72014dea0f5b17d4f8c7d94ba0f43df942c3
ollinieminen <otn987@gmail.com>
2019-07-14 07:21:59 -0700

    style background page

--- src/components/background/Background.js
+++ src/components/background/Background.js
@@ -8,10 +8,12 @@ import { FaLinkedin } from 'react-icons/fa';
 export default function Background() {
 
   return (
-    <section id='Background' className={[styles.section, 'page'].join(' ')}>
-      <section className='content'>
-        <section className={styles.background}>
-          <Title title='background' />
+    <section id='Background' className={[styles.backgroundPage, 'backgroundPage'].join(' ')}>
+    
+      <section className={styles.background}>
+        <Title title='background' />
+
+        <section className={styles.backgroundText}>
           <section>
             <ul className={styles.ulEducation}>
               <li>{education.degree}</li>
@@ -23,14 +25,15 @@ export default function Background() {
             <SkillMapper title={certificates.title} skills={certificates.skills} />
           </section>
         </section>
-        <section className={styles.social}>
-          <p>
+
+      </section>
+      <section className={styles.social}>
+        <p>
             Find my detailed work experience on LinkedIn
-          </p>
-          <a alt="Linked In" aria-label= "Linked In" rel= "noopener noreferrer" target = '_blank' href='https://www.linkedin.com/in/virpinieminen/'>
-            <FaLinkedin size={42} />
-          </a>
-        </section>
+        </p>
+        <a alt="Linked In" aria-label= "Linked In" rel= "noopener noreferrer" target = '_blank' href='https://www.linkedin.com/in/virpinieminen/'>
+          <FaLinkedin size={42} />
+        </a>
       </section>
 
 
