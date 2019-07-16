a49ffe9682e94d9501b867509b5330f6fc1e5970
ollinieminen <otn987@gmail.com>
2019-07-16 10:14:32 -0700

    styling fixes per feedback

--- src/components/expertise/Expertise.js
+++ src/components/expertise/Expertise.js
@@ -5,33 +5,34 @@ import Title from '../title/Title';
 import {
   FaRandom,
   FaGlobeAmericas,
-  FaBookReader,
+  FaBook,
   FaRegFileAlt,
-  FaRegStar
+  FaRegStar,
 } from 'react-icons/fa';
+
 
 export default function Expertise() {
 
   const expertise = [
     {
-      text: 'Ability to quickly grasp complex, technical solutions',
-      icon: <FaRandom size={48} />
+      text: ['Able to quickly', 'grasp complex,', 'technical', 'solutions'],
+      icon: <FaRandom size={36} />
     },
     {
-      text: 'Extensive experience in international business',
-      icon: <FaGlobeAmericas size={48} />
+      text: ['Extensive', 'experience in', 'international', 'business'],
+      icon: <FaGlobeAmericas size={36} />
     },
     {
-      text: 'Can translate tech talk into non technical language',
-      icon: <FaBookReader size={48} />
+      text: ['Able to translate', 'tech talk into', 'non-technical', 'language'],
+      icon: <FaBook size={36} />
     },
     {
-      text: 'Able to see past complexity to bring out the best',
-      icon: <FaRegStar size={48} />
+      text: ['Can see past', 'complexity to', 'bring out the', 'best'],
+      icon: <FaRegStar size={36} />
     },
     {
-      text: 'Copywrite and layout design',
-      icon: <FaRegFileAlt size={48} />
+      text: ['Both', 'copywriting', 'and layout', 'design'],
+      icon: <FaRegFileAlt size={36} />
     }
   ];
 
@@ -39,20 +40,22 @@ export default function Expertise() {
     return <li key={i}>
       <section className={styles.li}>
         {expertise.icon}
-        <p>
-          {expertise.text}
-        </p>
+        <ul className={styles.iconUl}>
+          {expertise.text.map((text, i)=><li key={i}>{text}</li>)}
+        </ul>
       </section>
     </li>;
   });
 
   return (
     <section id='Expertise' className={[styles.expertisePage, 'expertisePage'].join(' ')}>
-      <Title title='Expertise'/>
-
-      <ul className={styles.ul}>
-        {expertiseList}
-      </ul>
+      
+      <section className={styles.conent}>
+        <Title title='Expertise'/>
+        <ul className={styles.ul}>
+          {expertiseList}
+        </ul>
+      </section>
 
     </section>
   );
