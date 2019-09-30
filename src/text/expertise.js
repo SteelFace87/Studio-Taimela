596d50ce2c35c163c3a43a90e823379893f99124
ollinieminen <otn987@gmail.com>
2019-09-30 12:37:54 -0700

    clean up code in Expertise.js

---
+++ src/text/expertise.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  FaRandom,
+  FaGlobeAmericas,
+  FaComments,
+  FaFileAlt,
+  FaStar,
+} from 'react-icons/fa';
+
+export const expertise = [
+  {
+    text: ['Able to quickly', 'grasp complex,', 'technical', 'solutions'],
+    icon: <FaRandom size={36} />,
+  },
+  {
+    text: ['Extensive', 'experience in', 'international', 'business'],
+    icon: <FaGlobeAmericas size={36} />,
+  },
+  {
+    text: ['Translate', 'tech talk into', 'non-technical', 'language'],
+    icon: <FaComments size={36} />,
+  },
+  {
+    text: ['Can see past', 'complexity to', 'bring out the', 'best'],
+    icon: <FaStar size={36} />,
+  },
+  {
+    text: ['Both', 'copywriting', 'and layout', 'design'],
+    icon: <FaFileAlt size={36} />,
+  },
+];
