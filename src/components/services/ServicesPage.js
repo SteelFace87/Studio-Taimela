d33de334654a103256c0ac2641cc5f38b392906d
ollinieminen <otn987@gmail.com>
2019-10-02 08:35:21 -0700

    clean up ServicesPage

--- src/components/services/ServicesPage.js
+++ src/components/services/ServicesPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { services, aboutMe } from '../../text/services';
 import styles from './Services.css';
 import Title from '../title/Title';
 import ContentDevider from '../content-devider/ContentDevider';
-import { makeServicesList } from './makeServicesList';
+import ServicesList from './ServicesList';
+import AboutVirpi from './AboutVirpi';
 
 export default function Services() {
   return (
@@ -13,8 +13,8 @@ export default function Services() {
         className={[styles.servicesPage, 'servicesPage'].join(' ')}
       >
         <Title title="services" />
-        <ul className={styles.servicesList}>{makeServicesList(services)}</ul>
-        <p className={styles.aboutMe}>{aboutMe}</p>
+        <ServicesList />
+        <AboutVirpi />
       </main>
       <ContentDevider />
     </>
